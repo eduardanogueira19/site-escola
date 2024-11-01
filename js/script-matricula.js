d1935e97f2efa4e25db3f97a9658fb8b5fe2adeb
eduardanogueira19 <eduardanogueira812@gmail.com>
2024-11-01 20:06:01 -0300

    ajustando a responsividade

--- js/script-matricula.js
+++ js/script-matricula.js
@@ -27,3 +27,16 @@ function enviarParaWhatsApp() {
     // Abre o WhatsApp com a mensagem pré-preenchida
     window.open(urlWhatsApp, '_blank');
 }
+
+
+// Função abre e fecha menu em telas menores
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "../imagens/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "../imagens/close_white_36dp.svg";
+    }
+}

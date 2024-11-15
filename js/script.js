451458d9f6cad3c9b508bbeae1ed09fb6a9195ae
eduardanogueira19 <eduardanogueira812@gmail.com>
2024-11-15 20:28:13 -0300

    ajustes

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function acceptPolicy() {
     document.getElementById("cookie-banner").style.display = "none";
 }
 window.onload = function() {
-    if (!localStorage.getItem("policyAccepted")) { // Se não aceitou, exibe o banner
+    if (!localStorage.getItem("policyAccepted")) { 
       document.getElementById("cookie-banner").style.display = "flex";
     } else {
       document.getElementById("cookie-banner").style.display = "none";
@@ -27,7 +27,7 @@ showSlides(currentSlide);
 
 const currentCarouselIndex = {};
 function moveCarousel(carouselId, direction) {
-// Inicializa o índice se não existir
+
     if (!(carouselId in currentCarouselIndex)) {
         currentCarouselIndex[carouselId] = 0;
     }
@@ -36,38 +36,30 @@ function moveCarousel(carouselId, direction) {
     const carouselItems = carousel.querySelectorAll('.carousel-item');
     const totalItems = carouselItems.length;
 
-    // Dimensões e cálculo de itens visíveis
     const itemWidth = carouselItems[0].clientWidth;
     const carouselWidth = carousel.clientWidth;
     const itemsVisible = Math.floor(carouselWidth / itemWidth);
 
-    // Atualiza o índice do carrossel com base na direção
     currentCarouselIndex[carouselId] += direction;
 
-    // Impede que o índice vá além dos limites do carrossel
     if (currentCarouselIndex[carouselId] < 0) {
         currentCarouselIndex[carouselId] = 0;
     } else if (currentCarouselIndex[carouselId] > totalItems - itemsVisible) {
         currentCarouselIndex[carouselId] = totalItems - itemsVisible;
     }
 
-    // Define o offset para o carrossel
     let offset = -currentCarouselIndex[carouselId] * itemWidth;
 
-    // Ajuste para evitar o corte da última imagem
-    const extraMargin = carouselWidth % itemWidth; // Obtém a largura extra não utilizada
+    const extraMargin = carouselWidth % itemWidth;
     if (currentCarouselIndex[carouselId] === totalItems - itemsVisible && extraMargin !== 0) {
-        offset -= extraMargin; // Aplica o ajuste na última posição
+        offset -= extraMargin; 
     }
 
-    // Aplica o deslocamento ao carrossel
     carousel.style.transform = `translateX(${offset}px)`;
 
-    // Controle de visibilidade das setas
     const prevButton = document.querySelector(`#${carouselId} .prev`);
     const nextButton = document.querySelector(`#${carouselId} .next`);
 
-    // Mostra ou esconde as setas com base na posição
     prevButton.style.display = currentCarouselIndex[carouselId] === 0 ? 'none' : 'block';
     nextButton.style.display = currentCarouselIndex[carouselId] === totalItems - itemsVisible ? 'none' : 'block';
 }
